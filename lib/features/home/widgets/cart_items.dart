a14f127647534d23d57b8c34b382f2139ad3f5c6
Ahmed_Elgendy <1223048@dtu.edu.eg>
2026-09-16 22:22:49 +0300

    feat: add favorite functionality

--- lib/features/home/widgets/cart_items.dart
+++ lib/features/home/widgets/cart_items.dart
@@ -1,18 +1,32 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gap/flutter_gap.dart';
 import 'package:hungry_food_app/core/constants/app_assets.dart';
 import 'package:hungry_food_app/core/constants/app_colors.dart';
 
-class CartItems extends StatelessWidget {
-  const CartItems({super.key, this.rating = 3.5});
+class CartItems extends StatefulWidget {
+  const CartItems({super.key, this.rating = 3.5, this.isFavorite = false});
 
   final double rating; // Example rating value
+  final bool isFavorite;
+  @override
+  State<CartItems> createState() => _CartItemsState();
+}
 
+class _CartItemsState extends State<CartItems> {
+  late bool _isFavorite;
+  @override
+  void initState() {
+    super.initState();
+    _isFavorite = widget.isFavorite; // التهيئة الأولية
+  }
+
+  // Example favorite status
   IconData _getStarIcon(int index) {
     // index يبدأ من 0 حتى 4
-    if (rating >= index + 1) {
+    if (widget.rating >= index + 1) {
       return Icons.star; // نجمة كاملة
-    } else if (rating >= index + 0.5) {
+    } else if (widget.rating >= index + 0.5) {
       return Icons.star_half; // نصف نجمة
     } else {
       return Icons.star_border; // نجمة فارغة
@@ -61,14 +75,36 @@ class CartItems extends StatelessWidget {
                 Gap(5),
                 Text('\$35.00', style: TextStyle(color: AppColors.primary)),
                 Gap(5),
-                Row(
-                  children: List.generate(5, (starIndex) {
-                    return Icon(
-                      _getStarIcon(starIndex),
-                      color: Colors.amber,
-                      size: 16,
-                    );
-                  }),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: List.generate(5, (starIndex) {
+                          return Icon(
+                            _getStarIcon(starIndex),
+                            color: Colors.amber,
+                            size: 16,
+                          );
+                        }),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _isFavorite = !_isFavorite;
+                          });
+                        },
+                        child: Icon(
+                          _isFavorite
+                              ? CupertinoIcons.heart_fill
+                              : CupertinoIcons.heart,
+                          color: AppColors.primary,
+                          size: 22,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
