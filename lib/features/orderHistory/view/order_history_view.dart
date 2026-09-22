f9b00d2153d35c055803b372096f4df40de0b941
Ahmed_Elgendy <1223048@dtu.edu.eg>
2026-09-22 19:09:17 +0300

    feat: add Order History  view

--- lib/features/orderHistory/view/order_history_view.dart
+++ lib/features/orderHistory/view/order_history_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hungry_food_app/features/orderHistory/widgets/order_history_card.dart';
 
 class OrderHistoryView extends StatelessWidget {
   const OrderHistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Order History')));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          children: [
+            /// Cart Items List
+            Expanded(
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return OrderHistoryCard();
+                },
+              ),
+            ),
+
+            /// Total Section
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/orderHistory/widgets/order_history_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+import 'package:hungry_food_app/core/constants/app_assets.dart';
+import 'package:hungry_food_app/core/constants/app_colors.dart';
+import 'package:hungry_food_app/core/widgets/custom_button.dart';
+
+class OrderHistoryCard extends StatelessWidget {
+  const OrderHistoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(flex: 1, child: Image.asset(AppAsset.bergerImage)),
+                const Gap(10),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Burger",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text("Veggie Burger", textAlign: TextAlign.center),
+                      Text(
+                        "Quantity :  2",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            CustomButton(
+              buttonColor: AppColors.primary,
+              buttonText: "re  Order",
+              radius: 18,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
